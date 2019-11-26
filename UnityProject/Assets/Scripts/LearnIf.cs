59262938cfbb3672bc165ad3b87ecdb36d8682df
KID421 <suchild7@gmail.com>
2019-11-26 17:02:02 +0800

    完成動畫與音效控制

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    // 欄位
+    // 修飾詞 類型 名稱;
+    public bool test;
+
+    public bool weapon;
+
+    // Start 遊戲開始執行一次
+    private void Start()
+    {
+        if (true)
+        {
+            print("測試!");
+        }
+
+        // 如果
+        if (test)
+        {
+            // () 內部林值勾選會執行這裡
+            print("布林值勾選了!!!");
+        }
+        // 否則
+        else
+        {
+            // () 內部林值取消勾選會執行這裡
+            print("布林值取消勾選了!!!");
+        }
+
+        if (weapon)
+        {
+            print("AK47");
+        }
+        else
+        {
+            print("小刀");
+        }
+    }
+
+    // 一秒執行約 60 次
+    private void Update()
+    {
+        // Unity API
+        // 輸入.取得按鍵(按鍵列舉.名稱)
+        // 使用這個方法會得到一個布林值
+        // 玩家按下指定按鍵會得到 true 沒按則是 false
+        print(Input.GetKeyDown(KeyCode.Space));
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("往右走!!!");
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -13,25 +13,100 @@ public class Player : MonoBehaviour
     [Header("玩家名稱")]
     public string playerName = "KID";
 
-    // 陣列
-    public AudioClip[] sounds;
+    public AudioClip soundAtk;
+    public AudioClip soundJump;
+    public AudioClip soundGround;
+    public AudioClip soundDamage;
+    public AudioClip soundDead;
+
+    // 動畫控制器
+    public Animator ani;
+    // 音效來源 (喇叭)
+    public AudioSource aud;
 
     // 定義方法 method
     // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
     public void Move()
     {
-        print("移動!!!");
+        // 練習判斷按 D 輸出往右走
+        // 練習判斷按 A 輸出往左走
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("往右走");
+            ani.SetBool("跑步開關", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            ani.SetBool("跑步開關", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("往左走");
+            ani.SetBool("跑步開關", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            ani.SetBool("跑步開關", false);
+        }
+    }
+
+    public void Jump()
+    {
+        // 練習判斷按 Space 輸出跳躍
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ani.SetBool("跳躍開關", true);
+            aud.PlayOneShot(soundJump, 1.5f);
+        }
+    }
+
+    public void Attack()
+    {
+        // 練習判斷按 Mouse0 輸出攻擊
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("攻擊");
+            ani.SetTrigger("攻擊觸發");
+            aud.PlayOneShot(soundAtk, 1.5f);
+        }
+    }
+
+    public void Damage()
+    {
+        print("受傷");
+        ani.SetTrigger("受傷觸發");
+        aud.PlayOneShot(soundDamage, 1.5f);
+    }
+
+    public void Dead()
+    {
+        print("死亡");
+        ani.SetBool("死亡開關", true);
+        aud.PlayOneShot(soundDead, 2f);
     }
 
     // 事件 - 在特定時間會執行的方法
     // 開始：遊戲播放時執行一次
     private void Start()
     {
-        Move();
+        
     }
+
     // 更新：一秒執行約 60 次 (60FPS)
     private void Update()
     {
         Move();
+        Jump();
+        Attack();
+
+        // 測試
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Damage();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Dead();
+        }
     }
 }
